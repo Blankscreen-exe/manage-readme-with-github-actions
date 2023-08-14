b412f42c9bb3bf79a119810b5d2fac0c7425b422
Muhammad Hammad Hassan <51869983+Blankscreen-exe@users.noreply.github.com>
2023-08-14 15:00:22 +0500

    Update add_to_list.py

--- helpers/add_to_list.py
+++ helpers/add_to_list.py
@@ -1,7 +1,26 @@
 import argparse
 
 def add_line(file_path, file_name, file_link):
+    """
+    Add a formatted line to a file containing a link with a user-friendly name.
     
+    This function takes a file path, a file name, and a file link as input. The file name is
+    formatted to enhance its readability by replacing underscores with spaces and capitalizing
+    the first letter of each word. The formatted name is then used to create a markdown-style link
+    with the provided file link. The formatted line is appended to the specified file, followed
+    by a newline character.
+    
+    Parameters:
+        file_path (str): The path to the file where the formatted line will be appended.
+        file_name (str): The name of the file, which will be formatted for display.
+        file_link (str): The URL or link associated with the file.
+        
+    Example:
+        add_line('links.md', 'example_file.txt', 'https://example.com/files/example_file.txt')
+        
+    In the 'links.md' file, the following line will be appended:
+        - [Example File](https://example.com/files/example_file.txt)
+    """
     file_name = file_name.split(".")[0].replace("_", " ").title()
     
     text = f'- [{file_name}]({file_link})'
