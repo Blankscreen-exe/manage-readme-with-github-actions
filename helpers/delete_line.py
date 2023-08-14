d97b3440adab012873db87e792379644b06c1d84
Muhammad Hammad Hassan <51869983+Blankscreen-exe@users.noreply.github.com>
2023-08-14 14:54:43 +0500

    Update delete_line.py

--- helpers/delete_line.py
+++ helpers/delete_line.py
@@ -1,6 +1,34 @@
 import argparse
 
 def delete_matching_lines(file_path, search_string, count=0):
+    """
+    Delete lines containing a specified search string from a file.
+    
+    This function reads the content of the specified file and searches for lines
+    containing the given search string. Depending on the value of the 'count'
+    parameter, it performs different actions:
+    
+    - If 'count' is 0 or positive:
+      Deletes all occurrences of lines containing the search string, or only the first 'count'
+      occurrences if 'count' is greater than 0.
+      
+    - If 'count' is negative:
+      Deletes the last 'abs(count)' occurrences of lines containing the search string.
+    
+    The modified content is then written back to the file, effectively removing the
+    specified lines. The function provides feedback on the number of removed lines.
+    
+    Parameters:
+        file_path (str): The path to the file from which lines will be deleted.
+        search_string (str): The string to search for within the file's lines.
+        count (int, optional): The count of lines to delete. Positive values delete from
+            the start, negative values delete from the end. Default is 0 (delete all).
+            
+    Example:
+        delete_matching_lines('data.txt', 'obsolete', count=3)
+        
+    This will remove the last 3 lines containing the word 'obsolete' from 'data.txt'.
+    """
     try:
         with open(file_path, 'r') as file:
             lines = file.readlines()
